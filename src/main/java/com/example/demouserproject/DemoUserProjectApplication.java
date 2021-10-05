4c845266551737e11bb949661b7c18425d16b460
Alexander <alexander_reztsov@mail.ru>
2021-10-05 13:04:30 +0300

    init

---
+++ src/main/java/com/example/demouserproject/DemoUserProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demouserproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoUserProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoUserProjectApplication.class, args);
+    }
+
+}
